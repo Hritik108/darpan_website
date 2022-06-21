c93bb5f0e1dfd0d73425bdfc82da36b5adbbfcae
vikhil0609 <sallagargivikhil123@gmail.com>
2022-06-21 14:40:58 +0530

    transition in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,27 @@
 import React from "react";
 import "../assets/styles/navbar.css"
 import Logo from "../assets/images/Logo.jpg"
+import jQuery from "jquery";
 
 class Navbar extends React.Component{
 
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+      }
+
+      handleScroll(event){
+        let scrollTop = window.pageYOffset;
+        console.log(scrollTop)
+
+        if (scrollTop > 10){
+            jQuery(".Navbar-main").css({"position":"fixed",height:"60px"});
+        }
+
+        else{
+            jQuery(".Navbar-main").css({"position":"static",height:"80px"});
+        }
+    }
     render(){
 
         return(
