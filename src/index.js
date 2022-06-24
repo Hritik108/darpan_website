15af84eb0518c4d275e23fd4e25279a9ff4cdb63
vikhil0609 <75301866+vikhil0609@users.noreply.github.com>
2022-06-24 17:15:12 +0530

    router

--- src/index.js
+++ src/index.js
@@ -7,26 +7,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/homepage';
 import AboutUs from './components/AboutUs'
  
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-      <Route path="/" element={<Navbar />}>
-          <Route index element={<HomePage />} />
-          <Route path="/About" element={ <AboutUs />}  />
-          <Route path="/Events" element={ <AboutUs />}  />
-          <Route path="/Career" element={ <AboutUs />}  />
-          <Route path="/Contact" element={ <AboutUs />}  />
-      </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <BrowserRouter>
+  <Navbar />
+    <Routes>
+      <Route path="/" element={HomePage} />
+      <Route path="Home" element={< HomePage />} />
+      <Route path="About" element={<AboutUs />} />
+    </Routes>
+  </BrowserRouter>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
