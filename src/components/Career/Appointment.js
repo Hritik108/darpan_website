68cb5a1ad63456be0609e184a469a844329d3335
unknown <chauhanritik786@gmail.com>
2022-06-29 17:13:02 +0530

    counselling

--- src/components/Career/Appointment.js
+++ src/components/Career/Appointment.js
@@ -1,28 +1,35 @@
 import React from "react";
-
+import "../../assets/styles/appointment.css"
+import thinking from "../../assets/images/thinking.png"
+import { Button } from '@mui/material';
 class Appointment extends React.Component {
   render() {
     return (
       <div className="Appointment">
-        <div className="Career-left">
+        <div className="Appointment-left">
+         <img src={thinking}/>
+        </div>
+        <div className="Appointment-right">
           <div>
-            <p>Confused about Career Choices</p>
+            <p>Confused about <br></br>Career Choices</p>
           </div>
-          <div>
-            <button>Book an Appointment</button>
+         
+          {/* <div className="Appointment-left-list">
+          <ul>
+         <li>One to One Counselling</li>
+         <li>Learning Emotional Intelligence</li>
+         <li>Improving IQ</li>
+         <li>xyz</li>
+      </ul>
+          </div> */}
+
+          <div className="Appointment-right-button">
+            {/* <button>Book an Appointment</button> */}
+            <Button variant="contained">Book an Appointment</Button>
           </div>
+          
         </div>
 
-        <div className="Career-right">
-          <div className="Career-right-list">
-          <ul>
-         <li>Beetroot</li>
-         <li>Ginger</li>
-         <li>Potato</li>
-         <li>Radish</li>
-      </ul>
-          </div>
-        </div>
       </div>
     );
   }
