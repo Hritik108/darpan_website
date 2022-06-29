a2b151902e19debba069847628fa5c7308fcecdb
vikhil0609 <sallagargivikhil123@gmail.com>
2022-06-29 09:30:43 +0530

    changes made for gitignore

--- src/components/home/homeIndex.js
+++ src/components/home/homeIndex.js
@@ -1,11 +1,31 @@
 import React from "react";
+import "../../assets/styles/Home/homeindex.css"
+import { Button } from '@mui/material';
+import jQuery from 'jquery';
 
 class Homeindex extends React.Component{
 
     render(){
         return (
-            <div className="index-main">
-                
+            <div className="index-main" style={{"height":window.innerheight}}>
+                <div className="index-description">
+                    <div className="desc-main">
+                        <h3>Govardhan Eco-Village</h3>
+                        <h1>KNOW <strong>YOURSELF</strong>, <br />
+                            KNOW YOUR <strong>POTENTIAL</strong></h1>
+                    </div>
+                    <div className="desc-sub">
+                        <p>
+                        At Govardhan Ecovillage you lose yourself into the 
+                        labyrinths of your innermost self using the time
+                         honoured snaps of Ayurveda, Yoga and Meditation.
+                        </p>
+                    </div>
+                    <div className="index-buttons">
+                        <Button variant="contained" style={{ backgroundColor: "#06283D" }}>KNOW MORE</Button>
+                        {/* <Button variant="contained">Button</Button> */}
+                    </div>  
+                </div>
             </div>
         )
     }
