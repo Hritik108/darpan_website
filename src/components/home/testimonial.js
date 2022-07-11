b2c29f31ceb2f56cb867a6dad3463b8313bb07e2
vikhil0609 <sallagargivikhil123@gmail.com>
2022-07-11 16:12:56 +0530

    changes made in navbar

--- src/components/home/testimonial.js
+++ src/components/home/testimonial.js
@@ -6,13 +6,25 @@ import person_logo from "../../assets/images/person.png";
 
 class HomeTestimonial extends React.Component {
   render() {
+    // var settings = {
+    //   dots: true,
+    //   infinite: true,
+    //   speed: 500,
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1,
+    // };
     var settings = {
-      arrows: true,
       dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      autoplay: true,
+      centerMode: true,
+      adaptiveHeight: true,
+      arrows: true,
+      autoplaySpeed: 5000,
+      className: "slides",
     };
     return (
       <Slider {...settings}>
@@ -24,6 +36,42 @@ class HomeTestimonial extends React.Component {
             <div className="item-right">
               <header className="item-header">
                 <h1>Vikhil Bajanna Sallagargi</h1>
+                <h4>Software Engineer</h4>
+              </header>
+              <p>
+                &#8220; It is a long established fact that a reader will be
+                distracted by the readable content of a page when looking at its
+                layout. The point of using.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial">
+          <div className="carousel-item">
+            <div className="item-left">
+              <img alt="person logo" src={person_logo} />
+            </div>
+            <div className="item-right">
+              <header className="item-header">
+                <h1>Shubham Ramesh Gole</h1>
+                <h4>Software Engineer</h4>
+              </header>
+              <p>
+                &#8220; It is a long established fact that a reader will be
+                distracted by the readable content of a page when looking at its
+                layout. The point of using.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial">
+          <div className="carousel-item">
+            <div className="item-left">
+              <img alt="person logo" src={person_logo} />
+            </div>
+            <div className="item-right">
+              <header className="item-header">
+                <h1>Hritik Ramdhan Chauhan</h1>
                 <h4>Software Engineer</h4>
               </header>
               <p>
