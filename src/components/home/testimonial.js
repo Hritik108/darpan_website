156b0f39366c867d0b44fd0eb64c9de244231144
vikhil0609 <sallagargivikhil123@gmail.com>
2022-07-21 20:16:11 +0530

    changes made

--- src/components/home/testimonial.js
+++ src/components/home/testimonial.js
@@ -24,8 +24,6 @@ class HomeTestimonial extends React.Component {
       autoplaySpeed: 5000,
       className: "slides",
       arrows: true,
-      nextArrow: <SamplePrevArrow />,
-      prevArrow: <SamplePrevArrow />,
     };
     return (
       <Slider {...settings}>
