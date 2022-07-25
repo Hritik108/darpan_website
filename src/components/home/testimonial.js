387089f01bba19693baa002b59f96a840514e5c0
vikhil0609 <sallagargivikhil123@gmail.com>
2022-07-25 12:58:43 +0530

    optimization changes made

--- src/components/home/testimonial.js
+++ src/components/home/testimonial.js
@@ -14,91 +14,6 @@ import left_arrow from "../../assets/images/left_arrow.png";
 import smart_man from "../../assets/images/smart_man.png";
 import smart_man2 from "../../assets/images/smart_man2.png";
 
-// class HomeTestimonial extends React.Component {
-//   render() {
-//     function next_arrow() {
-//       return <img src={right_arrow} />;
-//     }
-
-//     function prev_arrow() {
-//       return <img src={left_arrow} />;
-//     }
-
-//     var settings = {
-//       dots: true,
-//       infinite: true,
-//       speed: 500,
-//       slidesToShow: 1,
-//       slidesToScroll: 1,
-//       autoplay: true,
-//       centerMode: true,
-//       adaptiveHeight: true,
-//       autoplaySpeed: 5000,
-//       className: "slides",
-//       // arrows: true,
-//       nextArrow: next_arrow(),
-//       prevArrow: prev_arrow(),
-//     };
-//     return (
-//       <Slider {...settings}>
-//         <div className="testimonial">
-//           <div className="carousel-item">
-//             <div className="item-left">
-//               <img alt="person logo" src={smart_man} />
-//             </div>
-//             <div className="item-right">
-//               <header className="item-header">
-//                 <h1>Naresh Rana</h1>
-//                 <h4>Software Engineer</h4>
-//               </header>
-//               <p>
-//                 &#8220; It is a long established fact that a reader will be
-//                 distracted by the readable content of a page when looking at its
-//                 layout. The point of using.
-//               </p>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="testimonial">
-//           <div className="carousel-item">
-//             <div className="item-left">
-//               <img alt="person logo" src={smart_man2} />
-//             </div>
-//             <div className="item-right">
-//               <header className="item-header">
-//                 <h1>Manish shukhla</h1>
-//                 <h4>Software Engineer</h4>
-//               </header>
-//               <p>
-//                 &#8220; It is a long established fact that a reader will be
-//                 distracted by the readable content of a page when looking at its
-//                 layout. The point of using.
-//               </p>
-//             </div>
-//           </div>
-//         </div>
-//         {/* <div className="testimonial">
-//           <div className="carousel-item">
-//             <div className="item-left">
-//               <img alt="person logo" src={smart_man} />
-//             </div>
-//             <div className="item-right">
-//               <header className="item-header">
-//                 <h1>Naresh Rana</h1>
-//                 <h4>Software Engineer</h4>
-//               </header>
-//               <p>
-//                 &#8220; It is a long established fact that a reader will be
-//                 distracted by the readable content of a page when looking at its
-//                 layout. The point of using.
-//               </p>
-//             </div>
-//           </div>
-//         </div> */}
-//       </Slider>
-//     );
-//   }
-// }
 function HomeTestimonial(props) {
   const items = [
     {
@@ -155,10 +70,6 @@ function HomeTestimonial(props) {
     </Carousel>
   );
 }
-
-// NextIcon={<img className="carousel-arrows" src={right_arrow} />}
-// PrevIcon={<img className="carousel-arrows" src={left_arrow} />}
-
 const Item = ({ name, description, full_description, imageSrc }) => {
   return (
     <div className="testimonial">
