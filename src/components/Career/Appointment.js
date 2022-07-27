4a2a341b968ad4d9a773eebcc9f1defeb0002392
vikhil0609 <sallagargivikhil123@gmail.com>
2022-07-27 17:13:00 +0530

    server updates

--- src/components/Career/Appointment.js
+++ src/components/Career/Appointment.js
@@ -8,8 +8,7 @@ import EQ from "../../assets/images/EQ.jpg";
 import IQ from "../../assets/images/IQ.jpg";
 
 // import monk from "../../assets/images/monk.jfif";
-// import { Button } from "@mui/material@material-ui/core";
-import { Button } from "@material-ui/core";
+import { Button } from "@mui/material";
 class Appointment extends React.Component {
   render() {
     return (
@@ -22,25 +21,32 @@ class Appointment extends React.Component {
             <div>
               <p className="Appointment-right-moto">
                 Confused about <br />
-                Career Choices
+                Career Choices?
               </p>
               <p className="Appointment-right-moto-desc">
-                Mi arcu metus ad. Tellus tempor ornare convallis sodales
-                vulputate. Felis inceptos taciti phasellus, aptent purus conubia
-                cubilia! Fusce ad ante senectus consequat vel diam fames curae;
-                odio feugiat quam eu. Dictum blandit, enim venenatis consectetur
-                nam. Dictumst risus fringilla lacinia mattis eros suscipit
+                Nowadays there are a lot of career options to choose from, to
+                choose what's the best for you or finding out the right one that
+                suits you has become a crack of a task. Nevertheless, you don't
+                have to worry about it, with us, it's now going to be a child's
+                play. Click the below button to get on-boarded on the track to
+                success.
               </p>
             </div>
 
             <div className="Appointment-right-button">
               {/* <button>Book an Appointment</button> */}
-              <Button
-                variant="contained"
-                style={{ backgroundColor: "#06283D", position: "static" }}
+              <a
+                href="/Contact"
+                style={{ textDecoration: "none" }}
+                target="_blank"
               >
-                Book an Appointment
-              </Button>
+                <Button
+                  variant="contained"
+                  style={{ backgroundColor: "#06283D", position: "static" }}
+                >
+                  Book an Appointment
+                </Button>
+              </a>
             </div>
           </div>
         </div>
@@ -53,24 +59,28 @@ class Appointment extends React.Component {
 
             <div className="Appointment-services1-2 Appointment-services-desc">
               <h1>Career Guidance</h1>
-              <h4>
-                One-to-one mentoring is the traditional model of mentoring in
-                which a more senior individual is paired with a more junior
-                individual in order to provide the younger person with guidance,
-                support, and encouragement{" "}
-              </h4>
+              <p>
+                Right advice and guidance at the turning points of life are like
+                the full moon days of a year, which is a privilege to possess.
+                Proper Mentoring and suggestions become the base of the building
+                of success & accelerate a person to reach the top of the world
+                of achievements. Darpan worli has vowed to provide the best
+                guidance possible to the youth seeking it.
+              </p>
             </div>
           </div>
 
           <div className="Appointment-services2">
             <div className="Appointment-services2-2 Appointment-services-desc">
               <h1>One to One Mentoring</h1>
-              <h4>
-                One-to-one mentoring is the traditional model of mentoring in
-                which a more senior individual is paired with a more junior
-                individual in order to provide the younger person with guidance,
-                support, and encouragement{" "}
-              </h4>
+              <p>
+                Right advice and guidance at the turning points of life are like
+                the full moon days of a year, which is a privilege to possess.
+                Proper Mentoring and suggestions become the base of the building
+                of success & accelerate a person to reach the top of the world
+                of achievements. Darpan worli has vowed to provide the best
+                guidance possible to the youth seeking it.{" "}
+              </p>
             </div>
             <div className="Appointment-image">
               <img alt="desc" src={mentoring} />
@@ -84,24 +94,31 @@ class Appointment extends React.Component {
 
             <div className="Appointment-services1-2 Appointment-services-desc">
               <h1>Improving Emotional Quotient</h1>
-              <h4>
-                One-to-one mentoring is the traditional model of mentoring in
-                which a more senior individual is paired with a more junior
-                individual in order to provide the younger person with guidance,
-                support, and encouragement{" "}
-              </h4>
+              <p>
+                The emotional quotient is the ability of an individual to deal
+                with emotions while interacting with people in our daily life,
+                it becomes vital for every self to be empathic, responsive &
+                listen carefully to others. All such qualities are traits of a
+                high EQ level which are now one of the most desired skills in
+                corporates as well as in personal life. EQ helps in life-making
+                & adds value to every activity both personal and professional. &
+                as it is said “IQ wins the race, EQ wind the life”
+              </p>
             </div>
           </div>
 
           <div className="Appointment-services2">
             <div className="Appointment-services2-2 Appointment-services-desc">
               <h1>Improving Intelligence Quotient</h1>
-              <h4>
-                One-to-one mentoring is the traditional model of mentoring in
-                which a more senior individual is paired with a more junior
-                individual in order to provide the younger person with guidance,
-                support, and encouragement{" "}
-              </h4>
+              <p>
+                The term IQ has been around the corner in recent years, research
+                has shown the higher the IQ level the more intelligent the
+                person is. Everyone nowadays wishes to have a higher IQ which is
+                only possible through Reading, Exploring g and continuous
+                learning At darpan worli, we take care of every individual's
+                growth academically and personally which helps improve IQ as
+                well as EQ
+              </p>
             </div>
             <div className="Appointment-image">
               <img alt="desc" src={IQ} />
